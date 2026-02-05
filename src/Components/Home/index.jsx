c8dc3d46ea7e6481d1b9c1b8a42330d0a1046867
michel-hategekimana <michelhategekimana@127gmail.com>
2026-02-05 15:42:16 +0200

    added custom colors

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -2,7 +2,7 @@ export function Home(){
     return (
          <>
          <div className=" h-[90vh] flex justify-center items-center">
-            <h1 className="bg-red-600">Welcome to home</h1>
+            <h1 className="bg-secondary">Welcome to home</h1>
          </div>
         </>
     )
