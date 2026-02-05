f78f33ec01b30c6991788707c69dd6b5973af183
Twizerimana <gedeontwizerimana6@gmail.com>
2026-02-05 13:41:59 +0300

    completed set up codebase

---
+++ src/Components/Nav/index.jsx
@@ -0,0 +1,9 @@
+export function NavigationBar(){
+    return (
+        <>
+         <div className="flex justify-center items-center bg-blue-500">
+            <h1>Nav bar heading</h1>
+         </div>
+        </>
+    )
+}
