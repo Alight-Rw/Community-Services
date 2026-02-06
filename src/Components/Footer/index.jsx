0b6475332f84e0d662abb08f546c66ba310db853
aliceniyomufasha <niyomufashaa94@gmail.com>
2026-02-06 12:46:57 +0200

    changes

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,9 @@
+
+import BottomFooter from "./BottomFooter.jsx"
 export function Footer(){
     return (
         <>
-         <div className="bg-blue-500 flex justify-center items-center">
-            <h1>Welcome to footer</h1>
-         </div>
+        <BottomFooter/> 
         </>
     )
 }
