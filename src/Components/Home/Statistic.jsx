7683904e1b1db60d2fb14f5b2a54fafb82a67788
mrkaboss <mrkaboss263@gmail.com>
2026-02-06 09:54:56 +0200

    my commity

--- src/Components/Home/Statistic.jsx
+++ src/Components/Home/Statistic.jsx
@@ -5,22 +5,22 @@ export function Statistic(){
             
             <div className="text-white border-r border-white pr-2 sm:pr-10 lg:pr-20 text-center">
                 <h3 className="text-lg sm:text-2xl lg:text-3xl">1,000</h3>
-                <p className="text-xs sm:text-base">avilble services</p>
+                <p className="text-xs sm:text-base">Avilble Services</p>
             </div>
 
             <div className="text-white border-r border-white pr-2 sm:pr-10 lg:pr-20 text-center">
                 <h3 className="text-lg sm:text-2xl lg:text-3xl">15k+</h3>
-                <p className="text-xs sm:text-base">server clientes</p>
+                <p className="text-xs sm:text-base">Server Clientes</p>
             </div>
 
             <div className="text-white border-r border-white pr-2 sm:pr-10 lg:pr-20 text-center">
                 <h3 className="text-lg sm:text-2xl lg:text-3xl">87%</h3>
-                <p className="text-xs sm:text-base">happy clientes</p>
+                <p className="text-xs sm:text-base">Happy Clientes</p>
             </div>
 
             <div className="text-white text-center">
                 <h3 className="text-lg sm:text-2xl lg:text-3xl">30+</h3>
-                <p className="text-xs sm:text-base">services categotys</p>
+                <p className="text-xs sm:text-base">Services Categotys</p>
             </div>
 
         </div>
