a0758def4938f9152affc7ccf7fee1914ead5cbc
munyentwali augustin <eagleaust456@gmail.com>
2026-02-06 14:02:45 +0200

    sliding cards

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,9 +1,10 @@
+import Slide from "./Slide";
+
+
 export function Home(){
     return (
          <>
-         <div className=" w-full px-componentPadding h-[90vh] flex justify-center items-center">
-            {/* all child components */}
-         </div>
+         <Slide />
         </>
     )
 }
