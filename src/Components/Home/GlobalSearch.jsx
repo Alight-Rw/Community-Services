c3ba47a1204f0c9624fa80332e98613284dd2c1c
Ben-Be-lab <coblataylordelete250@gmail.com>
2026-02-06 18:43:42 +0200

    updated codes for global search

--- src/Components/Home/GlobalSearch.jsx
+++ src/Components/Home/GlobalSearch.jsx
@@ -6,11 +6,11 @@ import { Search, MapPin, Menu, ChevronDown } from 'lucide-react';
 function GlobalSearch() {
   return (
     
-    <div className="flex justify-center mt-10 mb-10 md:mt-20 md:mb-20 px-4">
+    <div className="flex justify-center mt-10 mb-10 md:mt-20 md:mb-20  p-componentPadding ">
       
       
       
-      <div className="flex flex-col md:flex-row items-center bg-dark-light-secondary p-2 md:p-1.5 rounded-2xl md:rounded-full w-full max-w-5xl gap-2 md:gap-0">
+      <div className="flex flex-col md:flex-row items-center bg-[#7BB7FF] p-2 md:p-1.5 rounded-2xl md:rounded-full w-full gap-2 md:gap-0 ">
         
         
         <div className="w-full md:flex-1 flex items-center gap-2 px-4 bg-secondary text-white rounded-full h-12">
