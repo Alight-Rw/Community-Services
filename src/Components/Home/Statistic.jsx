e207658a4bf26b05b8eeebf0dcffb1f20cecd48e
mrkaboss <mrkaboss263@gmail.com>
2026-02-06 09:26:02 +0200

    my commity

--- src/Components/Home/Statistic.jsx
+++ src/Components/Home/Statistic.jsx
@@ -1,34 +1,26 @@
 export function Statistic(){
     return(
         <>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 grid-rows-1 bg-secondary justify-items-center px-4 py-8 gap-y-6">
+        <div className="grid grid-cols-4 bg-secondary justify-items-center px-2 py-6 gap-2 overflow-x-auto">
             
-            <div className="text-white border-b sm:border-b-0 sm:border-r border-white sm:pr-10 lg:pr-20 pb-4 sm:pb-0">
-                <div className="justify-items-center">
-                    <h3 className="text-2xl md:text-3xl">1,000</h3>
-                </div>
-                <p className="text-base md:text-xl">avilble services</p>
+            <div className="text-white border-r border-white pr-2 sm:pr-10 lg:pr-20 text-center">
+                <h3 className="text-lg sm:text-2xl lg:text-3xl">1,000</h3>
+                <p className="text-xs sm:text-base">avilble services</p>
             </div>
 
-            <div className="text-white border-b sm:border-b-0 sm:border-r border-white sm:pr-10 lg:pr-20 pb-4 sm:pb-0">
-                <div className="justify-items-center">
-                    <h3 className="text-2xl md:text-3xl">15k+</h3>
-                </div>
-                <p className="text-base md:text-xl">server clientes</p>
+            <div className="text-white border-r border-white pr-2 sm:pr-10 lg:pr-20 text-center">
+                <h3 className="text-lg sm:text-2xl lg:text-3xl">15k+</h3>
+                <p className="text-xs sm:text-base">server clientes</p>
             </div>
 
-            <div className="text-white border-b sm:border-b-0 sm:border-r border-white sm:pr-10 lg:pr-20 pb-4 sm:pb-0">
-                <div className="justify-items-center">
-                    <h3 className="text-2xl md:text-3xl">87%</h3>
-                </div>
-                <p className="text-base md:text-xl">happy clientes</p>
+            <div className="text-white border-r border-white pr-2 sm:pr-10 lg:pr-20 text-center">
+                <h3 className="text-lg sm:text-2xl lg:text-3xl">87%</h3>
+                <p className="text-xs sm:text-base">happy clientes</p>
             </div>
 
-            <div className="text-white">
-                <div className="justify-items-center">
-                    <h3 className="text-2xl md:text-3xl">30+</h3>
-                </div>
-                <p className="text-base md:text-xl">services categotys</p>
+            <div className="text-white text-center">
+                <h3 className="text-lg sm:text-2xl lg:text-3xl">30+</h3>
+                <p className="text-xs sm:text-base">services categotys</p>
             </div>
 
         </div>
