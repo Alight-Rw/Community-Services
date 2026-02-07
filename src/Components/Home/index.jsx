3cc7ac8695ff2b93a612a3274dbc6bc6934010c5
munyentwali augustin <eagleaust456@gmail.com>
2026-02-07 20:20:55 +0200

    Restore index.css and index.jsx from develop

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,10 +1,8 @@
-import Slide from "./Slide";
-
-
+import { Statistic } from "./Statistic"
 export function Home(){
     return (
          <>
-         <Slide />
+         <Statistic/>
         </>
 
 
