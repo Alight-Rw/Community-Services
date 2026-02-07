ca50dc53506002434933be06e3c60a31e6d308f2
Namasengesho <phocasnamasengesho74@gmail.com>
2026-02-07 12:28:23 +0200

    Top footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,8 +1,10 @@
 
 import BottomFooter from "./BottomFooter.jsx"
+import Footertop from "./FooterTop.jsx"
 export function Footer(){
     return(
       <>
+      <Footertop />
      <BottomFooter/>
     </>
     )
