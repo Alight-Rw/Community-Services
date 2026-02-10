9c09770e6d0f11996b7b2c4c7af2fd39750367e3
Bonane <bonanebusoke@gmail.com>
2026-02-10 10:23:20 +0200

    fix color on input

--- src/Components/Auth/LoginForm.jsx
+++ src/Components/Auth/LoginForm.jsx
@@ -42,14 +42,14 @@ function LoginForm() {
               <input
                 type="text"
                 placeholder="Username"
-                className="border rounded-lg border-primary px-4 py-2 focus:outline-secondary"
+                className="border rounded-lg bg-universal border-primary px-4 py-2 focus:outline-secondary"
               />
 
               <div className="relative">
                 <input
                   type={showPassword ? "text" : "password"}
                   placeholder="Password"
-                  className="border rounded-lg border-primary px-4 py-2 w-full focus:outline-secondary"
+                  className="border rounded-lg bg-universal border-primary px-4 py-2 w-full focus:outline-secondary"
                 />
 
                 <button
