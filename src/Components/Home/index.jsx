2b2e39c686df1d31ec621223ad58eb876b2c1b6c
munyentwali augustin <eagleaust456@gmail.com>
2026-02-10 13:08:10 +0200

    solve content

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -2,9 +2,11 @@ import { Statistic } from "./Statistic"
 
 import Hero from "./Hero";
 import Slide from "./Slide"
+import GlobalSearch from "./GlobalSearch";
 export function Home(){
     return (
-         <>
+         <> 
+         <GlobalSearch />
          <Hero />
          <Statistic/>
          <Slide />
