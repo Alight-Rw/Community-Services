9f4816b323ced3296f733d7c7cb072e0e3a0569c
aliceniyomufasha <niyomufashaa94@gmail.com>
2026-02-11 17:41:42 +0200

    changes

---
+++ src/Components/Service/ServiceButton.jsx
@@ -0,0 +1,27 @@
+
+import image from "../../assets/image.png";
+
+const ServiceButton = () => {
+
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
+  return (
+    <div className="flex gap-x-0.5 items-center border-2 border-blue-400 rounded-2xl w-[120px] p-2 my-[25px]">
+      <img src={image} alt="image" className="w-5 h-5 object-cover" />
+      
+      <button 
+        type="button" 
+        onClick={handleClick}
+        className="text-blue-500 text-[14px]"
+      >
+        Book Now
+      </button>
+
+    </div>
+  );
+}
+
+export default ServiceButton;
+
