645134d3c82b0d11b7cf364ff4a6261ddde54933
aliceniyomufasha <niyomufashaa94@gmail.com>
2026-02-11 11:18:56 +0200

    change

--- src/Components/Auth/index.jsx
+++ src/Components/Auth/index.jsx
@@ -1,7 +1,8 @@
-import AuthLayout from "./AuthLayout";
-import ChangePassword from "./ChangePassword";
 import LoginForm from "./LoginForm";
+import AuthLayout from "./AuthLayout";
 import RegisterForm from "./RegisterForm";
+import ChangePassword from "./ChangePassword";
+import AccountVerifiedForm from "./AccountVerifiedForm";
 
 export function Auth({ pathURL }) {
 
@@ -9,9 +10,9 @@ export function Auth({ pathURL }) {
     <div>
       <div className="absolute z-10 w-full">
         {pathURL === '/register' && (<RegisterForm />)}
-       {pathURL === '/login' && (<LoginForm />)}
-       {pathURL === '/change-password' && (<ChangePassword />)}
-       
+        {pathURL === '/login' && (<LoginForm />)}
+        {pathURL === '/change-password' && (<ChangePassword />)}
+        {pathURL === '/account-verified' && (<AccountVerifiedForm />)}
       </div>
       <AuthLayout />
     </div>
