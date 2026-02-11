4468bb3b032204b3737e710b27d3e0cb4aa3da6d
Ben-Be-lab <coblataylordelete250@gmail.com>
2026-02-11 10:34:48 +0200

    satus card codes

--- src/Components/Dashboard/content/index.jsx
+++ src/Components/Dashboard/content/index.jsx
@@ -1,10 +1,11 @@
 import DashboardNav from "./DashboardNav";
-
+import StatusCard from "./StatusCard";
 export function DashboardContent(){
     return(
         <>
         
         <DashboardNav />
+        <StatusCard />
         </>
     )
 }
