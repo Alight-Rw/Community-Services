741f5d44b740b6af024b172ee39a4a8d36356ba7
Ben-Be-lab <coblataylordelete250@gmail.com>
2026-02-11 17:11:49 +0200

    final codes

--- src/Components/Dashboard/content/index.jsx
+++ src/Components/Dashboard/content/index.jsx
@@ -1,6 +1,6 @@
 import DashboardNav from "./DashboardNav";
 import ServicesStatics from "./ServicesStatics";
-
+import StatusCard from "../content/StatusCard"
 export function DashboardContent(){
     return(
         <div className="pt-20 bg-universal"> 
