80e37f34be83af1b1add8039d62ffbf66b5c0de8
Jackson-tj74 <tuyikundejackson74@gmail.com>
2026-02-12 14:51:31 +0200

    Updated services page

--- src/Components/Client/Service/ServiceButton.jsx
+++ src/Components/Client/Service/ServiceButton.jsx
@@ -1,27 +1,44 @@
 
 import image from "../../../assets/image.png";
 
-const ServiceButton = () => {
+const ServiceButton = ({ disabled }) => {
 
   const handleClick = () => {
-    alert("Button clicked!");
+    if (!disabled) {
+      alert("Button clicked!");
+    }
   };
 
   return (
-    <div className="flex gap-x-0.5 items-center border-2 border-blue-400 rounded-2xl w-[120px] p-2 my-[25px] ">
-      <img src={image} alt="image" className="w-5 h-5 object-cover" />
-      
-      <button 
-        type="button" 
+    <div
+      className={`flex gap-x-0.5 items-center border-2 rounded-2xl w-[120px] p-2 my-[25px]
+      ${
+        disabled
+          ? "border-blue-200 bg-blue-50 opacity-60 cursor-not-allowed"
+          : "border-blue-400 cursor-pointer"
+      }`}
+    >
+      <img
+        src={image}
+        alt="image"
+        className={`w-5 h-5 object-cover ${disabled ? "opacity-50" : ""}`}
+      />
+
+      <button
+        type="button"
         onClick={handleClick}
-        className="text-blue-500 text-[14px] hover:bg-small-soft-blue "
+        disabled={disabled}
+        className={`text-[14px]
+          ${
+            disabled
+              ? "text-blue-300 cursor-not-allowed"
+              : "text-blue-500 hover:bg-small-soft-blue"
+          }`}
       >
         Book Now
       </button>
-
     </div>
   );
-}
+};
 
 export default ServiceButton;
-
