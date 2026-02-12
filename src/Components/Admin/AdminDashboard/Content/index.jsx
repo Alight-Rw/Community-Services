ccc3726349b900803d1a6f94b0a2ecc47ce923d4
aliceniyomufasha <niyomufashaa94@gmail.com>
2026-02-12 13:40:15 +0200

    change

--- src/Components/Admin/AdminDashboard/Content/index.jsx
+++ src/Components/Admin/AdminDashboard/Content/index.jsx
@@ -1,7 +1,11 @@
+import Paragraphy from "./Paragraphy";
+
 export function AdminDashboardContent(){
     return (
         <>
-        <h1>Welcome to admin pannel</h1>
+        <h1></h1>
+        <Paragraphy highlight={"Dashboard"} title={"Overview"} description={"Quick summary of key metrics, recent activities, and service performance"}/>
+
         </>
     )
 }
