317fa2a0d3d9faf3435ef118bad253df17978f6e
Jackson-tj74 <tuyikundejackson74@gmail.com>
2026-02-12 15:20:17 +0200

    Updated services page

--- src/Components/Client/Service/ServicesCards.jsx
+++ src/Components/Client/Service/ServicesCards.jsx
@@ -8,7 +8,7 @@ function ServicesPage() {
   {image: "/ServicesImage/ServiceImg4.png", title: 'Cleaning Services Group', category: 'Cleaning', description: "We provide reliable and professional cleaning services for homes and businesses. Our team ensures thorough, efficient, and high-quality cleaning to create a fresh, healthy, and spotless environment.", place: "KG 9 Avenue, Kigali", phoneNumber: "+250788888888", time: "08:00 AM - 18:00 PM", available: "Service Available"},
   {image: "/ServicesImage/ServiceImg5.png", title: 'Plumbing Solutions Ltd', category: 'Plumbing', description: "We provide professional plumbing services, including installations, repairs, and maintenance for residential and commercial properties. Our team delivers reliable solutions, quality workmanship, and prompt service to ensure your plumbing systems run smoothly and efficiently.", place: "KG 8 St Remera-Kabeza", phoneNumber: "+250788888888", time: "08:00 AM - 18:00 PM", available: "Service Unavailable"},
   {image: "/ServicesImage/ServiceImg6.png", title: 'Solar Tech Enterprise', category: 'Energy', description: "We provide reliable solar energy solutions, including installation, maintenance, and system upgrades for homes and businesses. Our goal is to deliver efficient, cost-effective, and sustainable energy systems that power your property with clean energy.", place: "NY 12 Rd, Rebero", phoneNumber: "+250788888888", time: "08:00 AM - 18:00 PM", available: "Service Available"},
-  {image: "/ServicesImage/ServiceImg7.png", title: 'KC Decorators Group', category: 'Decoration', description: "We provide professional decoration services for homes, offices, and events. Our team delivers creative designs, quality workmanship, and attention to detail to transform any space into a beautiful and inspiring environment.", place: "KG 9 Avenue, Kigali", phoneNumber: "+250788888888", time: "08:00 AM - 18:00 PM", available: "Service Available"},
+  {image: "/ServicesImage/ServiceImg7.png", title: 'KC Decorators Group', category: 'Decoration', description: "We provide professional decoration services for homes, offices, and events. Our team delivers creative designs, quality workmanship, and attention to detail to transform any space into a beautiful and inspiring environment.", place: "KG 9 Avenue, Kigali", phoneNumber: "+250788888888", time: "08:00 AM - 18:00 PM", available: "Service Unavailable"},
 
     
     
@@ -16,7 +16,7 @@ function ServicesPage() {
 
   return (
     <div className=" min-h-screen py-12 px-6 md:px-12 lg:px-20">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {items.map((item, index) => (
           <ServicesPageChild
             key={index}
