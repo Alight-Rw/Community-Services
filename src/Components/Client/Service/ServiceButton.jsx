582fe364fb1b0653b0076f3964ca5df815199822
Jackson-tj74 <tuyikundejackson74@gmail.com>
2026-02-12 10:26:42 +0200

    Updated service-page task

--- src/Components/Client/Service/ServiceButton.jsx
+++ src/Components/Client/Service/ServiceButton.jsx
@@ -10,30 +10,13 @@ const ServiceButton = ({ disabled }) => {
   };
 
   return (
-    <div
-      className={`flex gap-x-0.5 items-center border-2 rounded-2xl w-[120px] p-2 my-[25px]
-      ${
-        disabled
-          ? "border-blue-200 bg-blue-50 opacity-60 cursor-not-allowed"
-          : "border-blue-400 cursor-pointer"
-      }`}
-    >
-      <img
-        src={image}
-        alt="image"
-        className={`w-5 h-5 object-cover ${disabled ? "opacity-50" : ""}`}
-      />
-
-      <button
-        type="button"
+    <div className="flex gap-x-0.5 items-center border-2 border-blue-400 rounded-2xl w-[120px] p-2">
+      <img src={image} alt="image" className="w-5 h-5 object-cover" />
+      
+      <button 
+        type="button" 
         onClick={handleClick}
-        disabled={disabled}
-        className={`text-[14px]
-          ${
-            disabled
-              ? "text-blue-300 cursor-not-allowed"
-              : "text-blue-500 hover:bg-small-soft-blue"
-          }`}
+        className="text-blue-500 text-[14px]"
       >
         Book Now
       </button>
