428834b094d8197befd6ebd5a1729db2e4ff15f0
Jackson-tj74 <tuyikundejackson74@gmail.com>
2026-02-13 20:21:51 +0200

    page of confirm and fill information booking service task

--- src/Components/Client/Service/ServicesCardsChild.jsx
+++ src/Components/Client/Service/ServicesCardsChild.jsx
@@ -1,10 +1,20 @@
 
 import { MapPin, Phone, Clock, Star, Info, Banknote } from "lucide-react";
 import ServiceButton from "./ServiceButton";
+import { useNavigate } from "react-router-dom";
 
 function ServicesPageChild({ Img, title, ServiceName, description, place, phoneNumber, time, available, price }) {
   const isAvailable = available === "Service Available";
+  const navigate = useNavigate();
 
+  const handleBooking = () => {
+    if (isAvailable) {
+     
+      navigate("/booking", { 
+        state: { title, price, Img, ServiceName,description,place,time,phoneNumber } 
+      });
+    }
+  };
   return (
     <div className="flex flex-col rounded-[30px] p-4 h-full">
       <div className="w-full h-48 overflow-hidden rounded-t-[10px]">
@@ -51,47 +61,48 @@ function ServicesPageChild({ Img, title, ServiceName, description, place, phoneN
               <span className="text-[11px]">{time}</span>
 
             </div>
-            <div className="flex items-center gap-3  ">
-
-
-              <Banknote size={12} />
-              <span className="text-[11px]">{price}</span>
-            </div>
+           <div className="flex items-center gap-3 text-sm font-bold text-secondary pt-2">
+               <Banknote size={18} />
+                <span>{price}</span>
+               </div>
 
             <div>
 
             </div>
           </div>
 
-
           <div
-            className={`flex items-center gap-2 px-3 py-2 rounded-xl mb-6 border ${isAvailable ? "bg-[#E8F5E9] text-[#2E7D32] border-[#C8E6C9]/30"
-                : "bg-[#FFEBEE] text-red-600 border-[#FFCDD2]/30"
+            className={`flex items-center gap-2 px-3 py-2 rounded-xl mb-6 border ${isAvailable ? "bg-small-soft-green text-glass-green border-soft-green/30"
+                : "bg-[#FFEBEE] text-dark-red/60 border-soft-green/30"
               }`}
           >
             <div
-              className={`rounded-full p-0.5 ${isAvailable ? "bg-soft-yellow text-[#2E7D32]" : "bg-soft-yellow text-red-600"
+              className={`rounded-full p-0.5 ${isAvailable ? "bg-soft-yellow text-[#2E7D32]" : "bg-soft-yellow text-dark-red/60"
                 }`}
             >
-              <Info size={12} strokeWidth={3} />
+              <Info size={12} strokeWidth={3}  />
             </div>
             <span className="text-[11px] font-bold">
               {isAvailable ? "Service Available" : "Service Not Available"}
             </span>
           </div>
 
-          <div className="mt-auto pt-4 border-t border-hard-gray flex justify-center">
-            <ServiceButton
-              disabled={!isAvailable}
-              className={`w-full max-w-xs text-white py-2 px-4 rounded-lg font-bold ${isAvailable
-                  ? "bg-blue-500 hover:bg-blue-600 cursor-pointer"
-                  : "bg-blue-100 text-blue-300 cursor-not-allowed"
-                }`}
-            >
-              Book Now
-            </ServiceButton>
-          </div>
-
+         <div className="mt-auto pt-4 border-t border-hard-gray flex justify-center">
+          
+        <button
+          onClick={handleBooking} 
+          disabled={!isAvailable}
+          
+          className={`w-full max-w-xs text-white py-2 px-4 rounded-lg font-bold ${
+            isAvailable
+              ? "bg-secondary hover:bg-secondary cursor-pointer"
+              : "bg-blue-100 text-blue-300 cursor-not-allowed"
+          }`}>
+            Book now
+            </button>
+       
+         
+      </div>
         </div>
       </div>
     </div>
