9ba567dbd49f19df3aae213fa42304c360e3a6f5
Josue Byiringiro <josue.byiringiro@skrey-software.com>
2026-02-16 15:36:27 +0200

    Fixed project structure

---
+++ src/Components/Client/Dashboard/index.jsx
@@ -0,0 +1,17 @@
+import StatusCard from "./StatusCard";
+import Paragraphy from "../../Shared/Title";
+import ServicesStatics from "./ServicesStatics";
+import { ProfileSection } from "./ProfilSection";
+import DashboardNav from "../../Shared/DashboardNav";
+
+export function Dashboard() {
+  return (
+    <div className="pt-20 bg-universal">
+      <DashboardNav />
+      <Paragraphy highlight={"Dashboard"} title={"Overview"} description={ "Quick summary of key metrics, recent activities, and service performance" } />
+      <StatusCard />
+      <ServicesStatics />
+      <ProfileSection />
+    </div>
+  );
+}
