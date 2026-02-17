8b0b3dcd7cbae72b78cb5e4e166481c5b6e5fa67
michel-hategekimana <michelhategekimana@127gmail.com>
2026-02-17 18:19:28 +0200

    provide services process

--- src/Components/Client/Home/index.jsx
+++ src/Components/Client/Home/index.jsx
@@ -2,6 +2,7 @@ import { Statistic } from "./Statistic"
 import Hero from "./Hero";
 import Slide from "./Slide"
 import GlobalSearch from "../../Shared/GlobalSearch";
+import ServicesProcess from "./ServicesProcess";
 
 
 export function Home(){
@@ -11,6 +12,7 @@ export function Home(){
          <Hero />
          <Statistic/>
          <Slide />
+         <ServicesProcess />
         </>
     )
 }
