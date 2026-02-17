37b2d9cefc7a38ffb4f8ad41bb8b5fdb8bedbc2b
Jackson-tj74 <tuyikundejackson74@gmail.com>
2026-02-17 16:52:34 +0200

    remove unwanted contet

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -11,7 +11,7 @@ import { DashboardClientPage } from './Pages/Client/Dashboard';
 
 import ConfirmBookingPage from './Pages/Client/Booking';
 import { DashboardPage } from './Pages/Provider/Dashboard';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Contact from './Pages/Client/Contact';
 import { RequestedServicesPage } from './Pages/Client/RequestedSevices';
 import { WaitingServicesPage } from './Pages/Client/WaitingServices';
