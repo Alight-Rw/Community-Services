51d2f24d1ae446201946edc58c0d764437cc27fb
Namasengesho <phocasnamasengesho74@gmail.com>
2026-02-18 17:12:47 +0200

    work on global header

--- src/Pages/Client/Gallery.jsx
+++ src/Pages/Client/Gallery.jsx
@@ -1,11 +1,13 @@
 import ShowGallery from "../../Components/Client/WorkedGallery/ShowGallery";
+import GlobalHeader from "../../Components/Shared/GlobalHeader";
 import GlobalSearch from "../../Components/Shared/GlobalSearch";
 
 export function WorkGallery() {
   return (
     <>
       <GlobalSearch />
-      <div className=" px-[30px] md:px-componentPadding">
+      <GlobalHeader title='Our Resent Works' subtitle='Explore our portfolio of successfully completed projects showcasing our quality and dedication to every'/>
+      <div className=" py-16 px-[30px] md:px-componentPadding">
         <ShowGallery />
       </div>
     </>
