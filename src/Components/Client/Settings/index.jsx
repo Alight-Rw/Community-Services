cd4139458e2b571ff3ba25111db5e4516f316bc9
aliceniyomufasha <niyomufashaa94@gmail.com>
2026-02-19 10:15:29 +0200

    change

--- src/Components/Client/Settings/index.jsx
+++ src/Components/Client/Settings/index.jsx
@@ -1,9 +1,8 @@
-
-
 import React, { useState } from "react";
 import Paragraphy from "../../Shared/Title";
 import DashboardNav from "../../Shared/DashboardNav";
 import Sidebar from "../../Shared/Sidebar"; 
+import SettingCards from "./SettingCards";
 
 export function Settings() {
   const [isExpanded, setIsExpanded] = useState(false); 
@@ -36,13 +35,15 @@ export function Settings() {
             ${!isExpanded ? "ml-25 sm:33 md:ml-35 lg:ml-30 w-full xl:ml-11" : "ml-0"} /* Avoid going behind sidebar when collapsed */
           `}
         >
-          <div className="max-w-[1600px] mx-auto py-20 md:space-y-10">
+          <div className="max-w-[1600px] not-[]:mx-auto py-20 md:space-y-10">
             
           
             <Paragraphy 
-              highlight={"Settings"} 
-              description={"Manage your account preferences, system options, and personal settings."} 
+              highlight={"Account Settings"} 
+              description={"Quick Manage and update your personal account information settings"} 
             />
+            
+            <SettingCards/>
           </div>
         </main>
       </div>
