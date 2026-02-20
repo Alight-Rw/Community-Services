3291e5f06f4c80e4a88b9f2cef6716d820a2e7e6
aliceniyomufasha <niyomufashaa94@gmail.com>
2026-02-20 12:34:11 +0200

    change

--- src/Components/Shared/Title.jsx
+++ src/Components/Shared/Title.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Paragraphy = ({ highlight, title, description }) => {
   return (
-    <div className="  pt-4">
+    <div className="md:px-0 pt-4 ">
       <h2 className="font-bold text-[8px] sm:text-[12px] md:text-[18px]">
         <span className="underline">{highlight}</span> {title}
       </h2>
-      <p className="text-sm sm:text-base md:text-lg mt-2">
+      <p className="text-sm sm:text-base md:text-lg mt-2 w-[300px] md:w-full">
         {description}
         
       </p>
