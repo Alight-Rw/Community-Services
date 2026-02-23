220939a5338766c50cac4fa55555c303652080b3
Bonane <bonanebusoke@gmail.com>
2026-02-23 10:50:29 +0200

    paragraphe of edit-profil resolve responsive

--- src/Components/Shared/Title.jsx
+++ src/Components/Shared/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Paragraphy = ({ highlight, title, description }) => {
   return (
-    <div className="md:px-0 pt-4 ">
+    <div className="md:px-0 pl-5 pt-4 ">
       <h2 className="font-bold text-[16px] sm:text-[20px] md:text-[24px]">
         <span className="underline">{highlight}</span> {title}
       </h2>
