9ec3198455bb3623c409db0fb68db71527afcf66
Twizerimana <gedeontwizerimana6@gmail.com>
2026-02-23 13:09:24 +0300

    fix rejected

--- src/Components/Client/RejectedServices/index.jsx
+++ src/Components/Client/RejectedServices/index.jsx
@@ -6,7 +6,7 @@ import { RejectedServices, } from "./RejectedServices";
 import DashboardSearch from "../../Shared/DashboardSearch";
 
 export function RejectedSevices() {
-  const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(true);
 
   return (
     <div className="flex flex-col h-screen bg-universal overflow-hidden">
@@ -24,31 +24,23 @@ export function RejectedSevices() {
           <Sidebar isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
         </div>
 
-        <main
-          className={`
-              
-            transition-all duration-300 ease-in-out
-            ${!isExpanded ? "ml-25 sm:33 md:ml-35 lg:ml-30 w-full xl:ml-11" : "ml-0"}
-          `}
-        >
-          <div className="max-w-[1600px] mx-auto py-16 md:py-20 space-y-8 md:space-y-10 px-4 sm:px-6 md:px-8">
-            <Paragraphy
+       <main className='flex-1 pl-20 overflow-y-auto transition-all duration-300'>
+         <div className='px-8 md:px-0 pt-20 md:space-y-1 '>
+           <Paragraphy
               highlight={"Rejected Services"}
               description={
                 "Overview of services requested by the client that were not approved or denied."
               }
             />
-            <div className="px-0 mx-0 md:px-0 md:pr-14 py-10 w-[350px] md:w-full">
-              <DashboardSearch />
-            </div>
-
-
-             <div className='ml-8 md:ml-0 overflow-y-auto'>
+         </div>
+         <div className='px-8 md:px-0 md:pr-12 py-10 w-[350px] md:w-full'>
+            <DashboardSearch />
+          </div>
+          <div className='ml-8 md:ml-0 overflow-y-auto'>
                         <RejectedServices
                           width={isExpanded == true ? '1530px' : '1680px'}
                         />
                       </div>
-          </div>
         </main>
       </div>
     </div>
