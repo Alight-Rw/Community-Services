7586327f81fcd0fd555f71ff42d11c8555eea0bb
Jackson-tj74 <tuyikundejackson74@gmail.com>
2026-03-16 17:38:37 +0200

    client signup intergration

--- src/Services/APIsRequestService.js
+++ src/Services/APIsRequestService.js
@@ -9,6 +9,13 @@ export const APIsRequestService = {
   SignInAPI:async (data)=>{
     const headers = AuthHeader('json');
     return await fetch(`${BASE_URL}/auth/login`, { body:JSON.stringify(data), method: 'POST', headers })
+  },
+  
+
+  SignUpAPI:async (data)=>{
+    console.log(data)
+    const headers = AuthHeader('json');
+    return await fetch(`${BASE_URL}/auth/client-signup`, { body:JSON.stringify(data), method: 'POST', headers })
   }
   
 }
