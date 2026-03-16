34a9a79e94a6f370b4a643a1655c8bff9c7b71a3
Twizerimana <gedeontwizerimana6@gmail.com>
2026-03-16 13:46:50 +0300

    feature provider login

--- src/Services/APIsRequestService.js
+++ src/Services/APIsRequestService.js
@@ -5,4 +5,10 @@ export const APIsRequestService = {
     const headers = AuthHeader('json');
     return await fetch(`${BASE_URL}/api/welcome`, { method: 'GET', headers });
   },
+
+  SignInAPI:async (data)=>{
+    const headers = AuthHeader('json');
+    return await fetch(`${BASE_URL}/auth/login`, { body:JSON.stringify(data), method: 'POST', headers })
+  }
+  
 }
