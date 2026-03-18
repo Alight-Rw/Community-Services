36aba61772d3a7d94b5d4bd95b20e14e6e2c4282
Shadrack22788 <tuyizeresh@gmail.com>
2026-03-18 09:27:27 +0200

    Resolved conflicts

--- src/Services/APIsRequestService.js
+++ src/Services/APIsRequestService.js
@@ -40,5 +40,14 @@ FiechcategoryAPI:async() =>{
    
     const headers = AuthHeader('json');
     return await fetch(`${BASE_URL}/contact/message`, { body:JSON.stringify(data), method: 'POST', headers })
+    return await fetch(`${BASE_URL}/auth/login`, { body:JSON.stringify(data), method: 'POST', headers })
+  },
+  
+  GalleryAPI: async () => {
+    const headers = AuthHeader('json');
+    return await fetch(`${BASE_URL}/api/gallery`, {
+      method: 'GET',
+      headers
+    });
   }
 }
