e7f2694851ee49bb70e57b205a7044a8035081ed
Jackson-tj74 <tuyikundejackson74@gmail.com>
2026-03-19 14:31:26 +0200

    display and edit user profile intergration

--- src/Services/APIsRequestService.js
+++ src/Services/APIsRequestService.js
@@ -69,6 +69,22 @@ ForgotPasswordAPI: async (data) => {
     const headers = AuthHeader('json');
     headers.Authorization = `Bearer ${token}`
     return await fetch(`${BASE_URL}/auth/change-password`, { body: JSON.stringify(data), method: 'PATCH', headers })
- }
+ }, 
+ 
+  EditProfileAPI: async (data) => {
+    const headers = AuthHeader("form-data");
+    const formData = new FormData();
+    formData.append('name', data.name);
+    formData.append('email', data.email);
+    formData.append('phone', data.phone);
+    formData.append('location', data.location);
+    data.avatar && formData.append('avatar', data.avatar);
+
+    return fetch(`${BASE_URL}/auth/edit-profile`, {
+      body: formData,
+      method: "PATCH",
+      headers,
+    });
+  },
 
 }
